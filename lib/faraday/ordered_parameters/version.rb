2fd0be26a5092c4c26fb02242a474ad5dfc21859
Gabriel Prado <gabriel.prado@coffeebeantech.com>
2019-08-09 11:43:51 -0300

    Initial commit

---
+++ lib/faraday/ordered_parameters/version.rb
@@ -0,0 +1,5 @@
+module Faraday
+  module OrderedParameters
+    VERSION = '1.0.0'
+  end
+end
---
+++ spec/faraday/ordered_parameters_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Faraday::OrderedParameters do
+  it "has a version number" do
+    expect(Faraday::OrderedParameters::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
